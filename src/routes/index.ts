020854a0cf13b5b36d66b63d6f79dc236a112690
oriol <ogomez.s92@gmail.com>
2023-03-01 19:26:58 +0100

    refactor: remove unused imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,9 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import EndpointError from "..//enums/endpoint_error";
-import UserService from "../services/user_service";
-import User from "../interfaces/user";
-import StorageManager from "../services/storage_manager";
 import retrieveUser from './retrieve_user';
 import onlineUsers from './online_users';
 import newUser from './new_user';
 
 const router = Router();
-export const storageManager: StorageManager = new StorageManager();
 
 router.use('/', retrieveUser);
 router.use('/', onlineUsers);
